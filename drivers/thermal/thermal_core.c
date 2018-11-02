65b3a77ccb60744d23d6570d00e6441a9d2f5b30
Ram Chandrasekar <rkumbako@codeaurora.org>
2018-11-02 12:23:40 -0600

    drivers: thermal: Evaluate based on trip temperature
    With virtual sensors, temperature reads by different zones at different
    time can be different and hence can result in different set of
    thresholds. This can lead to a case where we can get interrupt for the
    same temperature back to back if the temperature is fluctuating.
    
    To avoid this, allow sensor drivers to read the temperature and provide
    that information along when notifying for trip notification. Thermal
    framework will now make sure all the thermal zones will use the same
    temperature in all thermal zones to evaluate.
    
    Bug: 129787335
    Bug: 129129453
    Change-Id: I10861be54f0948081a83a1e839d8f9401929c61b
    Signed-off-by: Ram Chandrasekar <rkumbako@codeaurora.org>
    Signed-off-by: Wei Wang <wvw@google.com>
    Signed-off-by: Lau <laststandrighthere@gmail.com>

--- drivers/thermal/thermal_core.c
+++ drivers/thermal/thermal_core.c
@@ -627,12 +627,12 @@ static void thermal_zone_device_reset(struct thermal_zone_device *tz)
 }
 
 void thermal_zone_device_update_temp(struct thermal_zone_device *tz,
-				enum thermal_notify_event event, int temp)
+				     enum thermal_notify_event event, int temp)
 {
 	int count;
 
 	if (atomic_read(&in_suspend) && (!tz->ops->is_wakeable ||
-		!(tz->ops->is_wakeable(tz))))
+					 !(tz->ops->is_wakeable(tz))))
 		return;
 
 	trace_thermal_device_update(tz, event);
